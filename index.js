7d8f765ff8c508fbc9a62dd88f02efb7afa5016b
Bishal721 <shresthabishal721@gmail.com>
2023-09-11 20:07:16 +0545

    Initial Commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://invent-app-dusky.vercel.app"],
+    origin: ["http://localhost:3000", "https://invent-app-fawn.vercel.app"],
     credentials: true,
   })
 );
