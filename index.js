7c1c3b83ff95865719474eae6abe6fe8ea8d3f43
Bishal721 <shresthabishal721@gmail.com>
2023-09-11 17:36:14 +0545

    Completed Project

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://Inventory-app.vercel.app"],
+    origin: ["http://localhost:3000", "https://invent-app-dusky.vercel.app"],
     credentials: true,
   })
 );
